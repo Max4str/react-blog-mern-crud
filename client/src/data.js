ce47e88a35a5e19acd1bc968bed94bfaed1e9246
Max Foerster <max4str@gmail.com>
2023-12-28 21:15:21 +1100

    Project Folder Structure, Routing, General CSS Styles, Navbar CSS, Footer, Error Page, Home/Posts, Post Detail/Single Post Page, Author & Category Posts

---
+++ client/src/data.js
@@ -0,0 +1,39 @@
+import Thumbnail1 from "./images/blog22.jpg";
+import Thumbnail2 from "./images/blog23.jpg";
+import Thumbnail3 from "./images/blog24.jpg";
+import Thumbnail4 from "./images/blog25.jpg";
+
+export const DUMMY_POSTS = [
+  {
+    id: "1",
+    thumbnail: Thumbnail1,
+    category: "education",
+    title: "This is the title of the first post on this blog.",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sapiente aut atque enim magnam deserunt, tenetur voluptas in. Cumque, at. Sit quaerat repellat officia reiciendis itaque dolorum dolor quae soluta?",
+    authorID: 3,
+  },
+  {
+    id: "2",
+    thumbnail: Thumbnail2,
+    category: "science",
+    title: "This is the title of the second post on this blog.",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sapiente aut atque enim magnam deserunt, tenetur voluptas in. Cumque, at. Sit quaerat repellat officia reiciendis itaque dolorum dolor quae soluta?",
+    authorID: 1,
+  },
+  {
+    id: "3",
+    thumbnail: Thumbnail3,
+    category: "education",
+    title: "This is the title of the third post on this blog.",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sapiente aut atque enim magnam deserunt, tenetur voluptas in. Cumque, at. Sit quaerat repellat officia reiciendis itaque dolorum dolor quae soluta?",
+    authorID: 13,
+  },
+  {
+    id: "4",
+    thumbnail: Thumbnail4,
+    category: "education",
+    title: "This is the title of the fourth post on this blog.",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia sapiente aut atque enim magnam deserunt, tenetur voluptas in. Cumque, at. Sit quaerat repellat officia reiciendis itaque dolorum dolor quae soluta?",
+    authorID: 11,
+  },
+];
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import Posts from "../components/Posts";
+
+const Home = () => {
+  return <Posts />;
+};
+export default Home;
